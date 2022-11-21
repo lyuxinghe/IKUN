2f49df5a6057fe685e0ca1a306b86afb805cf731
Lyuxing He <840267395@qq.com>
2022-11-20 23:41:45 -0600

    catkin_ws added

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/ur3/IKUN/catkin_ws/src
+# Build directory: /home/ur3/IKUN/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(rrbot_control)
+subdirs(rrbot_description)
+subdirs(rrbot_gazebo)
+subdirs(gazebo_tutorials)
