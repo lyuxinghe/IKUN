a2df16f0c0e40fb31f9f6fd09afce7ff61ccd676
Lyuxing He <840267395@qq.com>
2022-11-26 21:34:29 -0600

    cxk loaded in gazebo

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ur3/ece470/catkin_ws/src
+# Build directory: /home/ur3/ece470/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(CXK)
+subdirs(cxk_gazebo)
---
+++ catkin_ws/build/CXK/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ur3/ece470/catkin_ws/src/CXK
+# Build directory: /home/ur3/ece470/catkin_ws/build/CXK
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
