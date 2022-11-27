6cb35369ffe1ce3194a11dba4c057b8bf47e9a6a
Lyuxing He <82574277+lyuxinghe@users.noreply.github.com>
2022-11-26 21:28:42 -0600

    Delete catkin_ws directory

--- catkin_ws/build/rrbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ur3/IKUN/catkin_ws/src/rrbot_description
-# Build directory: /home/ur3/IKUN/catkin_ws/build/rrbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
